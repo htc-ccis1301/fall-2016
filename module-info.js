f5e049213988095a04375519591bb407fb17aa45
mbMosman <mbmosman@users.noreply.github.com>
2016-08-29 19:30:22 -0500

    mod 3 and logo updates

--- module-info.js
+++ module-info.js
@@ -1,9 +1,9 @@
 CCIS 1301 = {
 modules: [
   { course: "CCIS 1301", title: "Introduction to Web Development", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/intro", sort_order: 1, description: "This module provides an overview of web standards and architecture and an introduction to web development." },
-  { course: "CCIS 1301", title: "HTML Basics", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/html-basics", sort_order: 2, description: "An introduction to basic structural HTML tags." },
-  { course: "CCIS 1301", title: "CSS Basics", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/css-basics", sort_order: 3, description: "\n" },
-  { course: "CCIS 1301", title: "Graphics and Text Styling", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/graphics-text", sort_order: 4, description: "\n" },
+  { course: "CCIS 1301", title: "HTML Basics", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/html-basics", sort_order: 2, description: "An introduction to HTML syntax, structure, and common tags." },
+  { course: "CCIS 1301", title: "CSS Basics", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/css-basics", sort_order: 3, description: "This modules is an introduction to Cascading Style Sheets (CSS)." },
+  { course: "CCIS 1301", title: "Graphics and Text Styling", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/graphics-text", sort_order: 4, description: "This module focuses on adding interest to your web pages through the use of graphics and text styling." },
   { course: "CCIS 1301", title: "Advanced CSS", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/adv-css", sort_order: 5, description: "\n" },
   { course: "CCIS 1301", title: "Page Layout", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/page-layout", sort_order: 6, description: "\n" },
   { course: "CCIS 1301", title: "Introduction to Web Design", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/web-design", sort_order: 7, description: "\n" },
