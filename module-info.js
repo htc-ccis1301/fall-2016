93b6e34d3d772cc67dbcb7298fc06d0a0c6187d1
mbMosman <mbmosman@users.noreply.github.com>
2016-09-06 01:24:01 -0500

    added modules 4 & 5 & bootstrap

--- module-info.js
+++ module-info.js
@@ -1,20 +1,20 @@
 CCIS 1301 = {
 modules: [
-  { course: "CCIS 1301", title: "Introduction to Web Development", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/intro", sort_order: 1, description: "This module provides an overview of web standards and architecture and an introduction to web development." },
+  { course: "CCIS 1301", title: "Introduction to Web Development", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/intro", sort_order: 1, description: "An overview of web standards and architecture and an introduction to web development." },
   { course: "CCIS 1301", title: "HTML Basics", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/html-basics", sort_order: 2, description: "An introduction to HTML syntax, structure, and common tags." },
-  { course: "CCIS 1301", title: "CSS Basics", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/css-basics", sort_order: 3, description: "This modules is an introduction to Cascading Style Sheets (CSS)." },
+  { course: "CCIS 1301", title: "CSS Basics", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/css-basics", sort_order: 3, description: "This module is an introduction to Cascading Style Sheets (CSS)." },
   { course: "CCIS 1301", title: "Graphics and Text Styling", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/graphics-text", sort_order: 4, description: "This module focuses on adding interest to your web pages through the use of graphics and text styling." },
-  { course: "CCIS 1301", title: "Advanced CSS", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/adv-css", sort_order: 5, description: "\n" },
-  { course: "CCIS 1301", title: "Page Layout", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/page-layout", sort_order: 6, description: "\n" },
-  { course: "CCIS 1301", title: "Introduction to Web Design", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/web-design", sort_order: 7, description: "\n" },
-  { course: "CCIS 1301", title: "Responsive Web Design", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/resp-design", sort_order: 8, description: "\n" },
-  { course: "CCIS 1301", title: "Building & Styling Tables", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/tables", sort_order: 9, description: "\n" },
-  { course: "CCIS 1301", title: "Web Forms", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/forms", sort_order: 10, description: "\n" },
-  { course: "CCIS 1301", title: "Media & Interactivity", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/media", sort_order: 11, description: "\n" },
-  { course: "CCIS 1301", title: "Web Publishing & Testing", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/web-pub", sort_order: 12, description: "\n" },
+  { course: "CCIS 1301", title: "CSS Box Model", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/more-css", sort_order: 5, description: "An introduction to the CSS box model and additional CSS features to add color to your pages." },
+  { course: "CCIS 1301", title: "Page Layout", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/page-layout", sort_order: 6, description: "This module focuses on layout and positioning of elements on the page." },
+  { course: "CCIS 1301", title: "Introduction to Web Design", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/web-design", sort_order: 7, description: "An introduction to basic elements of web site design." },
+  { course: "CCIS 1301", title: "Responsive Web Design", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/resp-design", sort_order: 8, description: "An introduction to the technique of responsive web design and CSS media queries." },
+  { course: "CCIS 1301", title: "Building & Styling Tables", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/tables", sort_order: 9, description: "This module focuses on designing, building and styling HTML tables." },
+  { course: "CCIS 1301", title: "Web Forms", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/forms", sort_order: 10, description: "An introduction to designing, building, and styling HTML5 web forms." },
+  { course: "CCIS 1301", title: "Media & Interactivity", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/media", sort_order: 11, description: "In this module we'll learn to use audio, video and interactive effects to add interest to our web pages." },
+  { course: "CCIS 1301", title: "Web Publishing & Testing", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/web-pub", sort_order: 12, description: "This module discusses web publishing and testing for both usability and accessibility." },
   { course: "CCIS 1301", title: "Case Study: Pacific Trails Resort", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/cs-pac-trails", sort_order: 13, description: "This is a case study project, which is set up to simulate a real world project. In it, you will develop a web site for a business customer." },
-  { course: "CCIS 1301", title: "Introduction to Bootstrap", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/bootstrap", sort_order: 14, description: "\n" },
-  { course: "CCIS 1301", title: "Final Project & Wrap-up", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/wrap-up", sort_order: 15, description: "\n" }
+  { course: "CCIS 1301", title: "Introduction to Bootstrap", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/bootstrap", sort_order: 14, description: "An introduction to using the Bootstrap framework to aid in web site design and development." },
+  { course: "CCIS 1301", title: "Final Project & Wrap-up", moduleUrl: "http://htc-ccis1301.github.io/fall-2016/modules/wrap-up", sort_order: 15, description: "This module wraps up the course with a review of key topics and a final project." }
 ],
  prerequisites: [
 ]
